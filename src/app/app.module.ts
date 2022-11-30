9b5144871bda7fbf347a9fdb1ec75bc741fee1c3
LAPTOP-SM63HCM6\sarah <sarahsteenhuis@live.nl>
2022-11-30 16:03:11 +0100

    created cards and card container with basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardsContainerComponent } from './components/cards-container/cards-container.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsContainerComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
